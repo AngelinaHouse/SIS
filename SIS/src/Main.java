62a6e2a4b28cb35ecea85d92048f904d50299160
andrew.jackson <andrew.jackson@MU-C1-01.mullen.pvt.k12.co.us>
2021-01-26 10:01:27 -0700

    Added new method to read text file and added the text file.

--- SIS/src/Main.java
+++ SIS/src/Main.java
@@ -1,11 +1,27 @@
+import java.util.*;
+import java.io.*;
 
 public class Main
 	{
-
-		public static void main(String[] args)
+		ArrayList<Student> studentList = new ArrayList<Student>();
+		public static void main(String[] args) throws IOException
 			{
 				System.out.println("Welcome to the SIS");
-
+				readTextFile();
 			}
+		public static void readTextFile() throws IOException
+		{
+			//This method fills the student objects
+			
+			Scanner file = new Scanner(new File ("SIS Project Text File.txt"));
+			int times = file.nextInt();
+			 
+			for (int i = 0; i <= times; i++)
+			{
+				studentList.add(i);
+				
+			}
+		}
 
 	}
+
