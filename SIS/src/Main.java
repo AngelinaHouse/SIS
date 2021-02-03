be7635f9782d2c7abcf385cb8d982d53534cc1ba
MrPres50 <djax1110@gmail.com>
2021-02-03 09:44:21 -0700

    Troubleshooting SIS subfolder

--- SIS/src/Main.java
+++ SIS/src/Main.java
@@ -22,6 +22,167 @@ public class Main
 
 			}
 		}
+		
+		public static void gpaCalculator()
+		{
+			
+			for (int i = 0; i <= studentList.size(); i++)
+			{
+				double sum = 0;
+				double average = 0;
+				//grade 1 value
+				if (studentList.get(i).getClassGrade1().equals("A+"))
+				{
+					sum = sum + 4.0;
+				}
+				else if (studentList.get(i).getClassGrade1().equals("A"))
+				{
+					sum = sum + 4.0;
+				}
+				else if (studentList.get(i).getClassGrade1().equals("A-"))
+				{
+					sum = sum + 3.7;
+				}
+				else if (studentList.get(i).getClassGrade1().equals("B+"))
+				{
+					sum = sum + 3.3;
+				}
+				else if (studentList.get(i).getClassGrade1().equals("B"))
+				{
+					sum = sum + 3.0;
+				}
+				else if (studentList.get(i).getClassGrade1().equals("B-"))
+				{
+					sum = sum + 2.7;
+				}
+				else if (studentList.get(i).getClassGrade1().equals("C+"))
+				{
+					sum = sum + 2.3;
+				}
+				else if (studentList.get(i).getClassGrade1().equals("C"))
+				{
+					sum = sum + 2.0;
+				}
+				else if (studentList.get(i).getClassGrade1().equals("C-"))
+				{
+					sum = sum + 1.7;
+				}
+				else if (studentList.get(i).getClassGrade1().equals("D+"))
+				{
+					sum = sum + 1.3;
+				}
+				else if (studentList.get(i).getClassGrade1().equals("D"))
+				{
+					sum = sum + 1.0;
+				}
+				else if (studentList.get(i).getClassGrade1().equals("F"))
+				{
+					sum = sum + 0.0;
+				}
+				
+				//Grade 2 value
+				if (studentList.get(i).getClassGrade2().equals("A+"))
+				{
+					sum = sum + 4.0;
+				}
+				else if (studentList.get(i).getClassGrade2().equals("A"))
+				{
+					sum = sum + 4.0;
+				}
+				else if (studentList.get(i).getClassGrade2().equals("A-"))
+				{
+					sum = sum + 3.7;
+				}
+				else if (studentList.get(i).getClassGrade2().equals("B+"))
+				{
+					sum = sum + 3.3;
+				}
+				else if (studentList.get(i).getClassGrade2().equals("B"))
+				{
+					sum = sum + 3.0;
+				}
+				else if (studentList.get(i).getClassGrade2().equals("B-"))
+				{
+					sum = sum + 2.7;
+				}
+				else if (studentList.get(i).getClassGrade2().equals("C+"))
+				{
+					sum = sum + 2.3;
+				}
+				else if (studentList.get(i).getClassGrade2().equals("C"))
+				{
+					sum = sum + 2.0;
+				}
+				else if (studentList.get(i).getClassGrade2().equals("C-"))
+				{
+					sum = sum + 1.7;
+				}
+				else if (studentList.get(i).getClassGrade2().equals("D+"))
+				{
+					sum = sum + 1.3;
+				}
+				else if (studentList.get(i).getClassGrade2().equals("D"))
+				{
+					sum = sum + 1.0;
+				}
+				else if (studentList.get(i).getClassGrade2().equals("F"))
+				{
+					sum = sum + 0.0;
+				}
+				
+				//grade 3 value
+				if (studentList.get(i).getClassGrade3().equals("A+"))
+				{
+					sum = sum + 4.0;
+				}
+				else if (studentList.get(i).getClassGrade3().equals("A"))
+				{
+					sum = sum + 4.0;
+				}
+				else if (studentList.get(i).getClassGrade3().equals("A-"))
+				{
+					sum = sum + 3.7;
+				}
+				else if (studentList.get(i).getClassGrade3().equals("B+"))
+				{
+					sum = sum + 3.3;
+				}
+				else if (studentList.get(i).getClassGrade3().equals("B"))
+				{
+					sum = sum + 3.0;
+				}
+				else if (studentList.get(i).getClassGrade3().equals("B-"))
+				{
+					sum = sum + 2.7;
+				}
+				else if (studentList.get(i).getClassGrade3().equals("C+"))
+				{
+					sum = sum + 2.3;
+				}
+				else if (studentList.get(i).getClassGrade3().equals("C"))
+				{
+					sum = sum + 2.0;
+				}
+				else if (studentList.get(i).getClassGrade3().equals("C-"))
+				{
+					sum = sum + 1.7;
+				}
+				else if (studentList.get(i).getClassGrade3().equals("D+"))
+				{
+					sum = sum + 1.3;
+				}
+				else if (studentList.get(i).getClassGrade3().equals("D"))
+				{
+					sum = sum + 1.0;
+				}
+				else if (studentList.get(i).getClassGrade3().equals("F"))
+				{
+					sum = sum + 0.0;
+				}
+				average = sum/3;
+				studentList.get(i).setGPA(average);
+			}
+		}
 
 	}
 
