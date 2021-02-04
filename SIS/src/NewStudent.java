ad0c2b1a670e9ad3e79dbcb6a6ba220cd890982e
odhalbrook <33768369+odhalbrook@users.noreply.github.com>
2021-02-03 17:49:09 -0700

    Finished add/delete student

--- SIS/src/NewStudent.java
+++ SIS/src/NewStudent.java
@@ -5,6 +5,9 @@ public class NewStudent {
 	protected static Scanner uI = new Scanner(System.in);
 	protected static String fName = "";
 	protected static String lName = "";
+	protected static String [] classNames = new String [3];
+	protected static String [] classGrades = new String [3];
+
 	public static void addNewStudent()
 	{
 		
@@ -17,10 +20,53 @@ public class NewStudent {
 		System.out.println("Let's start with the basic information");
 		System.out.println();
 		makeName();
-		///next stuff
+		for (int p = 1; p < 4; p++)
+		{
+			makeClass(p);
+			System.out.println();
+			addGrade(p);
+		}
 		
+		//
+		Main.studentList.add(new Student(fName, lName, classNames[0], classGrades[0],classNames[1], classGrades[1],classNames[2], classGrades[2],0.0));
+		//
+		System.out.println();
+		System.out.println(Main.studentList.get(Main.studentList.size()-1) +" sucessfully created!");
+		System.out.println();
+
 	}
 	
+	private static void addGrade(int g) {
+		try
+		{
+			 System.out.println("Please enter the grade for " + classNames[g-1]);
+			 classGrades[g-1] = uI.nextLine();
+			
+		}
+		catch (Exception e)
+		{
+			System.out.println("Something went wrong, please try again");
+			addGrade(g);
+		}
+		
+	}
+
+	public static void makeClass(int c) {
+		
+		try
+		{
+			 System.out.println("Please enter the name of your period " + c);
+			 classNames[c-1] = uI.nextLine();
+			
+		}
+		catch (Exception e)
+		{
+			System.out.println("Something went wrong, please try again");
+			makeClass(c);
+		}
+		
+	}
+
 	public static void makeName()
 	{
 		try
